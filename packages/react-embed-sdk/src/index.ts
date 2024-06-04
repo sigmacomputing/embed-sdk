e7671888931d1a033bbfe8f83a1ad6be4a630bc4
Hiranmaya Gundu <hiranmaya@sigmacomputing.com>
2024-06-04 11:27:27 -0700

    feat: new api to load a sigma iframe

--- packages/react-embed-sdk/src/index.ts
+++ packages/react-embed-sdk/src/index.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   WorkbookErrorEvent,
   WorkbookLoadedEvent,
@@ -86,4 +86,44 @@ export function useWorkbookError(
     window.addEventListener("message", listener);
     return () => window.removeEventListener("message", listener);
   }, [iframeRef, onError]);
+}
+
+// This function can't be auto generated, but we can provide a
+// better abstraction. In the final state of this project, it would
+// be nice to see most functions auto generated but some
+// hand generated for better abstraction.
+
+/**
+ * A hook that returns a ref to be used with an iframe element, and the loading and error state of the embed.
+ *
+ * @example
+ * ```
+ * function MyEmbed() {
+ *  const { iframeRef, loading, error } = useSigmaIframe();
+ *  return (
+ *   <>
+ *     {loading && <p>Loading...</p>}
+ *     {error && <p>Error: {error.message}</p>}
+ *     <iframe
+ *       className={loading || error ? "hidden" : "show"}
+ *       ref={iframeRef}
+ *       src="https://sigmacomputing.app/embed"
+ *     />
+ *   </>
+ * );
+ * }
+ * ```
+ */
+export function useSigmaIframe() {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<WorkbookErrorEvent | null>(null);
+  const loadingCallback = useCallback(() => setLoading(false), []);
+  const errorCallback = useCallback((event: WorkbookErrorEvent) => {
+    setError(event);
+    setLoading(false);
+  }, []);
+  useWorkbookLoaded(iframeRef, loadingCallback);
+  useWorkbookError(iframeRef, errorCallback);
+  return { iframeRef, loading, error };
 }

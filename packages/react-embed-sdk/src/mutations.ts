a81e16c396b8c5749b032c89b402b99eb2aeb68f
Hiranmaya Gundu <hiranmaya@sigmacomputing.com>
2024-06-12 10:31:33 -0700

    feat: introduce all the mutations (#18)

--- packages/react-embed-sdk/src/mutations.ts
+++ packages/react-embed-sdk/src/mutations.ts
@@ -1,82 +1,68 @@
-import { useCallback, useEffect, useState } from "react";
 import {
   workbookVariablesList,
-  workbookCurrentVariablesListener,
   workbookVariablesUpdate,
+  workbookBookmarkCreate,
+  workbookBookmarkUpdate,
+  workbookFullscreenUpdate,
+  workbookSelectedNodeIdUpdate,
+  WorkbookBookmarkCreateEvent,
 } from "@sigmacomputing/embed-sdk";
 
-type CallBackState =
-  | {
-      state: "pending";
-      callback: (variables: Record<string, string>) => void;
-    }
-  | {
-      state: "success";
-      variables: Record<string, string>;
-    };
-
-/**
- * A hook that returns functions to get and set the variables in a workbook.
- * @example
- * ```
- * function MyEmbed() {
- *   const { getVariables, setVariables } = useWorkbookVariables(iframeRef);
- *   return (
- *    <>
- *     <button onClick={() => setVariables({ foo: "bar" })}>Set Variables</button>
- *     <button onClick={() => const variable = await getVariables()}>Get Variables</button>
- *     <iframe
- *       ref={iframeRef}
- *       src="https://app.sigmacomputing.com/embed"
- *     />
- *    <>
- *   );
- * }
- * ```
- */
-export const useWorkbookVariables = (
+export function getWorkbookVariables(
   iframeRef: React.RefObject<HTMLIFrameElement>,
-) => {
-  const [state, setState] = useState<CallBackState>();
-  const getVariables = useCallback(() => {
-    if (!iframeRef.current) {
-      throw new Error("iframe is not available");
-    }
-    workbookVariablesList(iframeRef.current);
-    const promise = new Promise((resolve) => {
-      setState({ state: "pending", callback: resolve });
-    });
-    return promise;
-  }, [iframeRef]);
-  const setVariables = useCallback(
-    (variables: Record<string, string>) => {
-      if (!iframeRef.current) {
-        throw new Error("iframe is not available");
-      }
-      workbookVariablesUpdate(iframeRef.current, variables);
-    },
-    [iframeRef],
-  );
-  useEffect(() => {
-    if (!iframeRef.current) return;
-    const listener = (event: MessageEvent) => {
-      if (!iframeRef.current) {
-        return;
-      }
-      workbookCurrentVariablesListener(event, iframeRef.current, (event) => {
-        if (state?.state === "pending") {
-          setState({ state: "success", variables: event.variables });
-          state.callback(event.variables);
-        }
-      });
-    };
-    window.addEventListener("message", listener);
-    return () => {
-      window.removeEventListener("message", listener);
-    };
-  }, [iframeRef, state]);
-  return {
-    getVariables,
-    setVariables,
-  };
-};
+) {
+  if (!iframeRef.current) {
+    throw new Error("iframe is not available");
+  }
+  workbookVariablesList(iframeRef.current);
+}
+
+export function updateWorkbookVariables(
+  iframeRef: React.RefObject<HTMLIFrameElement>,
+  variables: Record<string, string>,
+) {
+  if (!iframeRef.current) {
+    throw new Error("iframe is not available");
+  }
+  workbookVariablesUpdate(iframeRef.current, variables);
+}
+
+export function createWorkbookBookmark(
+  iframeRef: React.RefObject<HTMLIFrameElement>,
+  bookmark: WorkbookBookmarkCreateEvent,
+) {
+  if (!iframeRef.current) {
+    throw new Error("iframe is not available");
+  }
+  workbookBookmarkCreate(iframeRef.current, bookmark);
+}
+
+export function updateWorkbookBookmark(
+  iframeRef: React.RefObject<HTMLIFrameElement>,
+) {
+  if (!iframeRef.current) {
+    throw new Error("iframe is not available");
+  }
+  workbookBookmarkUpdate(iframeRef.current);
+}
+
+export function updateWorkbookFullscreen(
+  iframeRef: React.RefObject<HTMLIFrameElement>,
+  nodeId: string | null,
+) {
+  if (!iframeRef.current) {
+    throw new Error("iframe is not available");
+  }
+  workbookFullscreenUpdate(iframeRef.current, nodeId);
+}
+
+export function updateWorkbookSelectedNodeId(
+  iframeRef: React.RefObject<HTMLIFrameElement>,
+  nodeId: string,
+  nodeType: "element" | "page",
+) {
+  if (!iframeRef.current) {
+    throw new Error("iframe is not available");
+  }
+  workbookSelectedNodeIdUpdate(iframeRef.current, nodeId, nodeType);
+}

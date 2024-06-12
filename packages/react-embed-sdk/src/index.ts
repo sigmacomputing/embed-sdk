dfb99071c986e97ced634b0e574f9245852e5657
Hiranmaya Gundu <hiranmaya@sigmacomputing.com>
2024-06-12 10:30:48 -0700

    feat: introduce all the mutations

--- packages/react-embed-sdk/src/index.ts
+++ packages/react-embed-sdk/src/index.ts
@@ -3,5 +3,6 @@
 export type * from "@sigmacomputing/embed-sdk";
 export * from "./use-sigma-iframe";
 export * from "./use-page-height";
-export * from "./mutations";
+export * from "./use-workbook-variables";
 export * from "./wrappers";
+export * from "./mutations";
